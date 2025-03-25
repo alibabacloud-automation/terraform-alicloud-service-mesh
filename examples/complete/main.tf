a9280a2624814fe475de26153423d5279860a6fb
shanye <shanye.xzq@alibaba-inc.com>
2025-03-25 14:03:04 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -7,13 +7,15 @@ data "alicloud_zones" "default" {
 }
 
 module "vpc" {
-  source             = "alibaba/vpc/alicloud"
+  source  = "alibaba/vpc/alicloud"
+  version = "~> 1.11.0"
+
   create             = true
   vpc_name           = "tf-test-service-mesh"
   vpc_cidr           = "172.16.0.0/16"
   vswitch_name       = "tf-test-service-mesh"
   vswitch_cidrs      = ["172.16.0.0/21"]
-  availability_zones = [data.alicloud_zones.default.zones.0.id]
+  availability_zones = [data.alicloud_zones.default.zones[0].id]
 }
 
 resource "random_integer" "default" {
@@ -22,8 +24,8 @@ resource "random_integer" "default" {
 }
 
 resource "alicloud_log_project" "default" {
-  name        = "tf-example-${random_integer.default.result}"
-  description = "created by terraform"
+  project_name = "tf-example-${random_integer.default.result}"
+  description  = "created by terraform"
 }
 
 
@@ -34,10 +36,10 @@ data "alicloud_service_mesh_versions" "default" {
 module "example" {
   source                  = "../.."
   create_service_mesh     = true
-  name                    = var.name
+  name                    = "${var.name}--${random_integer.default.result}"
   vpc_id                  = module.vpc.this_vpc_id
   vswitche_ids            = [module.vpc.this_vswitch_ids[0]]
-  service_mesh_version    = reverse(data.alicloud_service_mesh_versions.default.versions).0.version
+  service_mesh_version    = reverse(data.alicloud_service_mesh_versions.default.versions)[0].version
   edition                 = var.edition
   force                   = var.force
   customized_zipkin       = var.customized_zipkin
